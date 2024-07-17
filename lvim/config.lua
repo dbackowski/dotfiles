3b96b6617b45eff2420f71e7357b2e18fd38e086
Damian Baćkowski <damian.backowski@chargify.com>
2024-07-17 16:45:06 +0200

    Add config for LunarVim

---
+++ lvim/config.lua
@@ -0,0 +1,27 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+--
+
+lvim.plugins = {
+  'sainnhe/sonokai',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Optionally configure and load the colorscheme
+    -- directly inside the plugin declaration.
+    vim.g.sonokai_enable_italic = true
+    vim.g.sonokai_style = 'maia'
+    vim.cmd.colorscheme('sonokai')
+  end
+}
+
+lvim.colorscheme = "sonokai"
+
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Terminal",
+  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
+  v = { "<cmd>2ToggleTerm size=30 direction=vertical<cr>", "Split vertical" },
+  h = { "<cmd>2ToggleTerm size=30 direction=horizontal<cr>", "Split horizontal" },
+}
